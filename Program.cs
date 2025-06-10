3b67cc891196d65db234ccd35ff6929fd46fb94d
Erik-Johansson13 <13ej1018@amal.se>
2025-06-10 10:30:20 +0200

    update

--- Program.cs
+++ Program.cs
@@ -1,5 +1,34 @@
 ﻿class Program {
-    public static void Main(string[] args) { 
+    public static void Main(string[] args) {
         //TODO: create a working roulett program
+        int money = 100;
+        int bet = 0;
+        while (true)
+        {
+            Console.WriteLine("Do you want to");
+            Console.WriteLine("(anything but 2). Gamble?");
+            Console.WriteLine("2. Not gamble?");
+            if (Console.ReadLine() == "2")
+            {
+                Console.Clear();
+                continue;
+            }
+            while (true)
+            {
+                Console.WriteLine("Enter your bet");
+                while (true)
+                {
+                    try
+                    {
+                        bet = Convert.ToInt32(Console.ReadLine());
+                    }
+                    catch
+                    {
+
+                    }
+                }
+
+            }
+        }
     }
 }
