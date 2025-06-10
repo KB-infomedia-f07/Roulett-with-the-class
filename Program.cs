3a0a09436ee387baf1349d2e1bfc341d61795759
jonathanlofgren1 <13JL0714@amal.se>
2025-06-10 10:31:24 +0200

    update

--- Program.cs
+++ Program.cs
@@ -1,5 +1,17 @@
 ﻿class Program {
-    public static void Main(string[] args) { 
+    public static void Main(string[] args) {
         //TODO: create a working roulett program
-    }
-}
+    
+
+    Random rand = new Random();
+    int playerNumber = 0;
+    string input = "";
+    bool isPlaying = true;
+    bool hasWon = false;
+
+        while (isPlaying) {
+            hasWon = false;
+        }
+
+
+
