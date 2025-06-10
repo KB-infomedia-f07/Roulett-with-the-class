f1f3a74c79a8b7b8e3616f78c042699ce3a05355
13MalFog <13mf0117@amal.se>
2025-06-10 10:32:17 +0200

    fdjisihfuhu

--- Program.cs
+++ Program.cs
@@ -1,5 +1,20 @@
 ﻿class Program {
     public static void Main(string[] args) { 
         //TODO: create a working roulett program
+
+        Random rand = new Random();
+        int pengar = 1000;
+        int avsluta = false;
+        int val1 = false;
+        int val2 = false;
+        int val 3 = false;
+
+        Console.WriteLine("Välkommen in! Sätt dig ner och spela en sväng roulette! Du KOMMER att förlora tillslut, bara så du vet!")
+
+        while (pengar > 0 && avsluta != true)
+        {
+            Console.WriteLine("Så då, vad vill du göra?")
+
+        }
     }
 }
