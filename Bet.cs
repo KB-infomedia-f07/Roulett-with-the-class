e75b0370d92e428a3db13762d6b338f0ef0200a1
Erik-Hard1 <13EH1104@amal.se>
2025-06-10 11:09:48 +0200

    AHHHHHHHHHHHHHHHH

---
+++ Bet.cs
@@ -0,0 +1,21 @@
+﻿class Bet
+{
+    //Hur mycket som beattst
+    public int BetValue { get; set; }
+
+    public int BetType { get; set; }
+
+    public int BettedNumber { get; set; }
+
+    public int BettedColor { get; set; }
+
+
+
+    public Bet(int betVal, int betType, int bettedNum, int bettedC)
+    {
+        BetValue = betVal;
+        BetType = betType;
+        BettedNumber = bettedNum;
+        BettedColor = bettedC;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,16 +4,31 @@
 
         Random rand = new Random();
         int pengar = 1000;
-        int avsluta = false;
-        int val1 = false;
-        int val2 = false;
-        int val 3 = false;
+        bool avsluta = false;
+        bool betting = true;
+     
+        int val1;
+        int val2;
+        int val3;
 
-        Console.WriteLine("Välkommen in! Sätt dig ner och spela en sväng roulette! Du KOMMER att förlora tillslut, bara så du vet!")
+        Console.WriteLine("Välkommen in! Sätt dig ner och spela en sväng roulette! Du KOMMER att förlora tillslut, bara så du vet!");
 
         while (pengar > 0 && avsluta != true)
         {
-            Console.WriteLine("Så då, vad vill du göra?")
+            Console.WriteLine("Så då, vad vill du göra?\n1. Nummer\n2. Färg\n 3. Jämn/Udda");
+            while (betting == true)
+            {
+                val1 = Convert.ToInt32(Console.ReadLine());
+                
+            }
+            
+            
+
+        }
+
+        void AddBet(int bettedAmount)
+        {
+            //Det finns en klass "Bet" som denna funktionen ska skapa objekt med upp till 3.
 
         }
     }
